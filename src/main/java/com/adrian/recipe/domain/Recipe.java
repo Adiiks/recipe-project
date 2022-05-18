8ba2d1da801eda729d2cc7ad9a8a67f2a40adf68
Adiks <apacholak97@interia.pl>
2022-05-18 11:39:34 +0200

    using setters for JPA bidirectional relationships

--- src/main/java/com/adrian/recipe/domain/Recipe.java
+++ src/main/java/com/adrian/recipe/domain/Recipe.java
@@ -118,6 +118,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
@@ -142,5 +143,10 @@ public class Recipe {
 
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
+    }
+
+    public void addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
     }
 }
