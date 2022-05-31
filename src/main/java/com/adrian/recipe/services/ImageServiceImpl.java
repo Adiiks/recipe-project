c90adefe4a2f81951a1d2899287b3d7eebe007a2
Adiks <apacholak97@interia.pl>
2022-05-31 08:40:35 +0200

    persisting images to database

--- src/main/java/com/adrian/recipe/services/ImageServiceImpl.java
+++ src/main/java/com/adrian/recipe/services/ImageServiceImpl.java
@@ -1,12 +1,43 @@
 package com.adrian.recipe.services;
 
+import com.adrian.recipe.domain.Recipe;
+import com.adrian.recipe.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        try {
+
+            Recipe recipe = recipeRepository.findById(recipeId)
+                    .orElseThrow(() -> new RuntimeException("Recipe not found. ID:" + recipeId));
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+        } catch (IOException exception) {
+            log.error("Error occurred", exception);
+
+            exception.printStackTrace();
+        }
     }
 }
