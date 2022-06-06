130da3618b4f140ea2ad1a5a85c690da638c9b2c
Adiks <apacholak97@interia.pl>
2022-06-06 09:21:08 +0200

    Spring MVC controller advice

--- src/test/java/com/adrian/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/adrian/recipe/controllers/RecipeControllerTest.java
@@ -37,7 +37,8 @@ public class RecipeControllerTest {
 
         recipeController = new RecipeController(recipeService);
 
-        mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(recipeController)
+                .setControllerAdvice(new ExceptionHandlerController()).build();
 
         recipe = new Recipe();
         recipe.setId(ID);
